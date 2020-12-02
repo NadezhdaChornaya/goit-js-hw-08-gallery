01d8635db4ee083f498a94b397a5955d7fc6b442
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2020-12-02 20:54:48 +0200

    fixet components

--- components/closeModal.js
+++ components/closeModal.js
@@ -1,12 +1,12 @@
 import { refs } from "../refs/refs.js";
 
-export const onCloseModal = function () {
+export const onCloseModal = () => {
     refs.modalWindow.classList.remove("is-open");
     refs.img.src = "";
     refs.img.alt = "";
 }
 
-export const onEscapeClose = function (e) {
+export const onEscapeClose = (e) => {
     if (e.code === "Escape") {
         onCloseModal();
     };
